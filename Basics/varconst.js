08f6d3aad434d564280b40b4ba580e9c98247b30
adilsiddiqui70786 <siddiquiadil70786@gmail.com>
2023-10-08 19:42:31 +0530

    "variables and constant"

---
+++ Basics/varconst.js
@@ -0,0 +1,15 @@
+const id = 234;  // changes not allowwed.
+let username = "Mohd Adil";
+var email = "siddiquiadil70786@gmail.com";
+city = "Allahabad"    // bad programmming 
+
+/* 
+  prefer not to use VAR keyword
+  due to issue of block and functional scope.
+*/
+
+
+let state;  // undefined
+
+
+console.table([id, username, email, city, state]);
