36c52d51a264e959c41f9393aa03070532ad82cc
adilsiddiqui70786 <siddiquiadil70786@gmail.com>
2023-10-17 20:37:53 +0530

    conversion of datatypes

---
+++ Basics/conversionoperation.js
@@ -0,0 +1,28 @@
+// let score = true
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)  // this is convertion of any variables 
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33a" => NaN
+// True = 1
+
+let isLoggedIn = 0
+
+let booleanIsloggedIn = Boolean(isLoggedIn);
+console.log(booleanIsloggedIn);
+
+
+// 1 => true; 0 => false
+// "" => false
+// "Adil" => true
+
+let num = 33
+
+let Stringnum = String(num);
+console.log(typeof Stringnum);
---
+++ Basics/datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all js code as newer version
+
+// alert ( 3 + 3 ) // we are using node.js, nnot browser
+
+
+let name = "Mohd Adil" // string
+let age = 20; // number - range 2 power 53
+// bigInt 
+
+
+/* 
+Boolean = true ;  // true or false
+null // standalone value     // object
+undefined // not assigned a value 
+symbol // uniqueness */
+
+
+//object
+
+console.log(typeof "A");
+console.log(typeof null);
+
+
+// to chekc any type of object - 
