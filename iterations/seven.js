642c3656c821652ab4d1d59a70a130636d47c29d
adilsiddiqui70786 <siddiquiadil70786@gmail.com>
2024-01-18 19:48:49 +0530

    Map

--- iterations/seven.js
+++ iterations/seven.js
@@ -1,9 +1,18 @@
 const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-// MAp : It is similar to forEach but it returns a new array automatically.
+// MAp : It is similar to forEach  but it returns a new array automatically.
 
-const newNums = myNums.map((num) => {
-	return (num = num + 10);
-});
+// const newNums = myNums.map((num) => {
+// 	return (num = num + 10);
+// });
+
+// console.log(newNums);
+
+// Chainig : It is a way to combine multiple methods together.
+
+const newNums = myNums
+	.map((num) => num + 10)
+	.map((num) => num * 2)
+	.filter((num) => num > 20);
 
 console.log(newNums);
