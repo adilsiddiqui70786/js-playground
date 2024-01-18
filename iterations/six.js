947f0478475035950486c6cea0aa23def981861b
adilsiddiqui70786 <siddiquiadil70786@gmail.com>
2024-01-18 19:01:32 +0530

    Filters

---
+++ iterations/six.js
@@ -0,0 +1,12 @@
+const teachers = ["Assaf", "Josh", "Andrew", "Justine", "Zach"];
+
+teachers.forEach((item) => {
+	console.log(item);
+});
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+numbers.forEach((num) => {
+	let square = num * num;
+	console.log(`The Square of ${num} is = ${square}`);
+});
