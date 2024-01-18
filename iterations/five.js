37438353bcc86933a65debf7d41d4d5ce995daad
adilsiddiqui70786 <siddiquiadil70786@gmail.com>
2024-01-18 18:50:17 +0530

    forEach Loop

--- iterations/five.js
+++ iterations/five.js
@@ -13,3 +13,54 @@ function PrintMe(item) {
 }
 
 course.forEach((item) => PrintMe(item));
+
+course.forEach((item, index, arr) => {
+	console.log(item, index, arr);
+});
+
+const myCoding = [
+	{
+		id: 1,
+		name: "HTML",
+	},
+	{
+		id: 2,
+		name: "CSS",
+	},
+	{
+		id: 3,
+		name: "JS",
+	},
+	{
+		id: 4,
+		name: "PHP",
+	},
+	{
+		id: 5,
+		name: "Python",
+	},
+	{
+		id: 6,
+		name: "Java",
+	},
+	{
+		id: 7,
+		name: "C++",
+	},
+	{
+		id: 8,
+		name: "C#",
+	},
+	{
+		id: 9,
+		name: "C",
+	},
+	{
+		id: 10,
+		name: "Ruby",
+	},
+];
+
+myCoding.forEach((item) => {
+	console.log(item.id, item.name);
+});
