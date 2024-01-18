c098287e4afa9853bc111d9f97b93128cdf739fe
adilsiddiqui70786 <siddiquiadil70786@gmail.com>
2024-01-18 19:20:48 +0530

    Filters

--- iterations/six.js
+++ iterations/six.js
@@ -35,3 +35,62 @@ console.log(newNums);
 // });
 
 // console.log(newNums);
+
+const books = [
+	{
+		title: "Book One",
+		genre: "Fiction",
+		published: 2010,
+	},
+	{
+		title: "Book Two",
+		genre: "Non-Fiction",
+		published: 2011,
+	},
+	{
+		title: "Book Three",
+		genre: "Historical Fiction",
+		published: 2012,
+	},
+	{
+		title: "Book Four",
+		genre: "Non-Fiction",
+		published: 2013,
+	},
+	{
+		title: "Book Five",
+		genre: "Science",
+		published: 2014,
+	},
+	{
+		title: "Book Six",
+		genre: "Non-Fiction",
+		published: 2015,
+	},
+	{
+		title: "Book Seven",
+		genre: "Fiction",
+		published: 2016,
+	},
+	{
+		title: "Book Eight",
+		genre: "Non-Fiction",
+		published: 2017,
+	},
+	{
+		title: "Book Nine",
+		genre: "Fiction",
+		published: 2018,
+	},
+	{
+		title: "Book Ten",
+		genre: "Non-Fiction",
+		published: 2019,
+	},
+];
+
+const userBooks = books.filter((book) => {
+	return book.genre === "Fiction";
+});
+
+console.log(userBooks);
