b296eab2cf64459a9f439b0b558094d28feb3c12
adilsiddiqui70786 <siddiquiadil70786@gmail.com>
2024-01-18 19:11:28 +0530

    Filters

--- iterations/six.js
+++ iterations/six.js
@@ -1,12 +1,27 @@
-const teachers = ["Assaf", "Josh", "Andrew", "Justine", "Zach"];
+// const teachers = ["Assaf", "Josh", "Andrew", "Justine", "Zach"];
 
-teachers.forEach((item) => {
-	console.log(item);
+// teachers.forEach((item) => {
+// 	console.log(item);
+// });
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// numbers.forEach((num) => {
+// 	let square = num * num;
+// 	console.log(`The Square of ${num} is = ${square}`);
+// });
+
+//For each does not return anything.
+
+// Filter: It returns a new array with the elements that pass the condition.
+
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// const newNums = myNums.filter((num) => num > 4);
+// console.log(newNums);
+
+const newNums = myNums.filter((num) => {
+	return num == 4;
 });
 
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-numbers.forEach((num) => {
-	let square = num * num;
-	console.log(`The Square of ${num} is = ${square}`);
-});
+console.log(newNums);
